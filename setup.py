8d3e6ed7db43cbfffc60b51580d0b6b25e0de762
Suzin You <suzinyou.sy@gmail.com>
2020-10-15 20:24:45 +0900

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
-from src.kobert import __version__
+from article_sentiment.kobert import __version__
 
 
-setup(name='kobert',
+setup(name='article_sentiment',
       version=__version__,
-      url='https://github.com/SKTBrain/KoBERT',
+      url='https://github.com/suzinyou/ubi_article_sentiment',
       license='Apache-2.0',
-      author='Heewon Jeon',
-      author_email='madjakarta@gmail.com',
-      description='Korean BERT pre-trained cased (KoBERT) ',
-      packages=['kobert', ],
+      author='Suzin You & Heewon Jeon',
+      author_email='suzinyou.sy@gmail.com',
+      description='Article sentiment classification with Korean BERT pre-trained cased (KoBERT) ',
+      packages=['article_sentiment', ],
       long_description=open('README.md', encoding='utf-8').read(),
       zip_safe=False,
       include_package_data=True,
