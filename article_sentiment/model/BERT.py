f8ece2b1b06455eb44b3ed6cdd367c6143a99d22
Suzin You <suzinyou.sy@gmail.com>
2021-03-22 16:06:03 +0900

    add another fc layer to classifier

--- article_sentiment/model/BERT.py
+++ article_sentiment/model/BERT.py
@@ -13,9 +13,13 @@ class BERTClassifier(nn.Module):
         self.bert = bert
         self.dr_rate = dr_rate
 
-        self.classifier = nn.Linear(hidden_size, num_classes)
-        if dr_rate:
+        if dr_rate is not None and dr_rate != 0.0:
             self.dropout = nn.Dropout(p=dr_rate)
+
+        self.fc1 = nn.Linear(hidden_size, hidden_size // 2)
+        self.relu = nn.ReLU()
+
+        self.fc2 = nn.Linear(hidden_size // 2, num_classes)
 
     def gen_attention_mask(self, token_ids, valid_length):
         attention_mask = torch.zeros_like(token_ids)
@@ -28,10 +32,12 @@ class BERTClassifier(nn.Module):
 
         _, pooler = self.bert(input_ids=token_ids, token_type_ids=segment_ids.long(),
                               attention_mask=attention_mask.float().to(token_ids.device))
-        if self.dr_rate:
-            out = self.dropout(pooler)
-        else:
-            out = pooler
 
-        out = self.classifier(out)
+        out = self.fc1(pooler)
+        out = self.relu(out)
+        if self.dr_rate:
+            out = self.dropout(out)
+        # else:
+        #     out = pooler
+        out = self.fc2(out)
         return out
